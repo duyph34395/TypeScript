bf4a5deccefd98f1d4b6e8a9274455b912f700f4
duyph34395 <duytvph34395@fpt.edu.vn>
2025-09-23 19:49:26 +0700

    lab3

---
+++ lab3/b1/funtion_arow.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    if (a === void 0) { a = 5; }
+    if (b === void 0) { b = 10; }
+    return a + b;
+}
+console.log("Sum:", sum());
+var multiply = function (a, b) {
+    if (a === void 0) { a = 10; }
+    if (b === void 0) { b = 13; }
+    return a * b;
+};
+console.log("multiply:", multiply());
---
+++ lab3/b1/funtion_arow.ts
@@ -0,0 +1,6 @@
+function sum(a:number=5,b:number=10) :number{
+    return a+b;
+}
+console.log("Sum:",sum());
+const multiply = (a: number=10, b: number=13): number => a * b;
+console.log("multiply:",multiply());
